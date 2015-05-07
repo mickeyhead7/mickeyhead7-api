d5715bc3a67737b876d1337dee603d1ab3598b37
Mark Head <mickeyhead7@gmail.com>
2015-05-07 06:32:34 +0100

    Added active database scope & trait

---
+++ src/Database/ActiveTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Responsible\Api\Database;
+
+trait ActiveTrait
+{
+
+    /**
+     * Apply scope to all queries
+     */
+    public static function bootActiveTrait()
+    {
+        static::addGlobalScope(new ActiveScope);
+    }
+
+    /**
+     * Get the active column
+     *
+     * @return string
+     */
+    public function getActiveColumn()
+    {
+        return defined('static::ACTIVE_COLUMN') ? static::ACTIVE_COLUMN : 'active';
+    }
+
+    /**
+     * Get the qualified active column
+     *
+     * @return string
+     */
+    public function getQualifiedActiveColumn()
+    {
+        return $this->getTable().'.'.$this->getActiveColumn();
+    }
+
+    /**
+     * Provide a method to get results with any active state
+     *
+     * @return mixed
+     */
+    public static function withDrafts()
+    {
+        return with(new static)->newQueryWithoutScope(new ActiveScope);
+    }
+
+}
